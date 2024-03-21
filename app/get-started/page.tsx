4d49e739d3fb4e7773c34be45e25e960eaf5a95b
Robert Otieno <robertotieno.or@gmail.com>
2024-03-21 09:40:13 +0300

    removed console

--- app/get-started/page.tsx
+++ app/get-started/page.tsx
@@ -28,7 +28,6 @@ export default function GetStarted() {
   };
 
   const onSubmit = async (e: { preventDefault: () => void }) => {
-    console.log(formData);
     setIsSubmitting(true);
     e.preventDefault();
     try {
